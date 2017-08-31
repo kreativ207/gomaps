e94a79a7a66535fe6ac179830b1d8fd034c12e78
kreativ207 <nikolaynigorodov@gmail.com>
2017-08-31 19:30:25 +0300

    new 3

--- protected/config/main.php
+++ protected/config/main.php
@@ -52,7 +52,7 @@ return array(
 		// uncomment the following to use a MySQL database
 
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=gmaps',
+			'connectionString' => 'mysql:host=localhost;dbname=gomaps',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => '',
